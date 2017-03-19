6146179c66fdafbeeffd191f07c02db3f2c83f23
Alex Stonehouse <alex.stonehouse@gmail.com>
2017-03-19 15:11:03 +0100

    Added script for codesigning on travis

---
+++ travis_setup_codesigning.sh
@@ -0,0 +1,14 @@
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo "Preparing Code Signing certificate"
+    $KEYCHAIN_PASSWORD=gsOX#$eZ#xPZYRTNSst9
+    export CERTIFICATE_P12=Certificate.p12;
+    echo $SIGNING_CERTIFICATE | base64 — decode > $CERTIFICATE_P12;
+    echo "Decoded p12"
+    export KEYCHAIN=build.keychain;
+    security create-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN;
+    security default-keychain -s $KEYCHAIN;
+    security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN;
+    echo "Unlocked keychain"
+    security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+    echo "Imported signing certificate"
+fi
