cf167f66c92ae9c48acac8bffd64971575b7cad8
Alex Stonehouse <alex.stonehouse@gmail.com>
2017-03-20 20:28:18 +0100

    Scaffolding of iOS app

---
+++ T-Minus-iOS/CountdownListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CountdownListViewController.swift
+//  T-Minus
+//
+//  Created by Alexander Stonehouse on 20/03/17.
+//  Copyright © 2017 Alexander Stonehouse. All rights reserved.
+//
+
+import Cocoa
+
+class CountdownListViewController: UICollectionViewController {
+
+}
---
+++ T-Minus-iOS/CreateCountdownViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateCountdownViewController.swift
+//  T-Minus
+//
+//  Created by Alexander Stonehouse on 20/03/17.
+//  Copyright © 2017 Alexander Stonehouse. All rights reserved.
+//
+
+import Cocoa
+
+class CreateCountdownViewController: UIViewController {
+
+}
